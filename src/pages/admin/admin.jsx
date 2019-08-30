ec149e97111f1f7119b014ba8f6c597df7372526
HeiHelloWorld <1689791513@qq.com>
2019-08-30 23:28:18 +0800

    button test

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+import { BrowserRouter,Route,Switch,Redirect } from "react-router-dom"
+
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
+
+export default class Admin extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/" component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
